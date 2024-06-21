afc7704aa091470dd74e76e5b93f63ca9e2421f8
Lidiya <simutina.1994@mail.ru>
2024-06-21 20:55:00 +0300

    Изменена основная логика Добавлен класс коробка

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,5 +4,11 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(12121);
+        System.out.println(222);
+        System.out.println(333);
+    }
+
+    public static void doSome() {
+        System.out.println(1);
     }
 }
